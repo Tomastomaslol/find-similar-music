475006a0fd5e8312776a054e37e9eca46633d6a5
Tomastomaslol <tomasnygren.854@gmail.com>
2016-06-07 15:10:40 +0100

    can view chief keef records

--- server.rb
+++ server.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
+require 'spotify-client'
 
+SpotifyClient = Spotify::Client.new
 
 set :haml, :format => :html5
 
 get '/' do
-  haml :index
+	albums = SpotifyClient.artist_albums("15iVAtD3s3FsQR4w1v6M0P")
+	puts albums.count
+	haml :index, :locals => {:albums => albums["items"] }
 end
