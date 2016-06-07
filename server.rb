98dbae664a3a303c6f93e28868b0a4d59f1ec63e
Tomastomaslol <tomasnygren.854@gmail.com>
2016-06-07 15:59:58 +0100

    search part added for the lolz

--- server.rb
+++ server.rb
@@ -7,6 +7,13 @@ set :haml, :format => :html5
 
 get '/' do
 	albums = SpotifyClient.artist_albums("15iVAtD3s3FsQR4w1v6M0P")
-	puts albums.count
 	haml :index, :locals => {:albums => albums["items"] }
+end
+
+
+post '/search/' do
+	puts params['search']
+	relatedartists = SpotifyClient.related_artists("15iVAtD3s3FsQR4w1v6M0P")
+	puts relatedartists
+	haml :search, :locals => {:albums => relatedartists["artists"]}
 end
